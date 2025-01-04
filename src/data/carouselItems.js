716de60bf4198e16f115a7422afcf2fde947b12c
Franklin Ohaegbulam <46662771+frankiefab100@users.noreply.github.com>
2025-01-04 17:21:45 +0100

    Create carouselItems.js

---
+++ src/data/carouselItems.js
@@ -0,0 +1,23 @@
+export const ITEMS = [
+  {
+    id: 1,
+    desktop: "/assets/desktop-image-hero-1.jpg",
+    mobile: "/assets/mobile-image-hero-1.jpg",
+    heading: "Discover innovative ways to decorate",
+    body: "We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.",
+  },
+  {
+    id: 2,
+    desktop: "/assets/desktop-image-hero-2.jpg",
+    mobile: "/assets/mobile-image-hero-2.jpg",
+    heading: "We are available all across the globe",
+    body: "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we're in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today.",
+  },
+  {
+    id: 3,
+    desktop: "/assets/desktop-image-hero-3.jpg",
+    mobile: "/assets/mobile-image-hero-3.jpg",
+    heading: "Manufactured with the best materials",
+    body: "Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office.",
+  },
+];
